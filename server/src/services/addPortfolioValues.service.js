1811d426be2de80cc79141e56062c97ee7db87fd
Aashna <aashnanarang33@gmail.com>
2022-01-17 13:07:01 -0500

    Add job to calculate and save portfolio values daily

---
+++ server/src/services/addPortfolioValues.service.js
@@ -0,0 +1,14 @@
+var cron = require('node-cron');
+const { portfolioValuesService } = require('../services');
+
+// job automatically runs every day at 12 am (00 seconds, 00 minutes, 00 hours)
+const task = cron.schedule('00 00 00 * * *', () => {
+    try {
+        portfolioValuesService.addPortfolioValuesJob();
+    } catch (error) {
+        console.log("Skip job " + error + Date());
+    }
+    console.log("Ran " + Date());
+})
+
+module.exports = task;
