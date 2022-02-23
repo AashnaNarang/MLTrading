893b986abe2182d239bfe2d1bebc3aa03a23b2c6
danihashweh <55706559+danihashweh@users.noreply.github.com>
2022-02-23 14:02:51 -0500

    Revert "updated working version"
    This reverts commit 19a3665ab9a0f093cbf87f8801d048b5024c92c0.

--- client/src/components/SecurityBox.js
+++ client/src/components/SecurityBox.js
@@ -1,65 +1,86 @@
-import React, {useState, Component} from 'react';
+import React, { useState, Component } from 'react';
 import '../SecurityBox.css'
-import {Modal} from "react-bootstrap";
+import SecuritiesModal from "./SecuritiesModal";
+import { Modal } from "react-bootstrap";
 import Button from "@material-ui/core/Button";
 
+let modal;
+
 function getNumberOfShares(shares) {
-    if (shares === 1)
-        return "share"
-    else
-        return "shares"
+  if (shares === 1)
+    return "share"
+  else
+    return "shares"
 }
 
 class SecurityBox extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            showModal: false
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    }
+  }
+
+  createModal = () => {
+    return (
+      <Modal
+        show={this.state.showModal}
+        close={() => this.setState({ showModal: false })}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">Logout</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Are you sure to Logout?</h4>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => this.setState({ showModal: false })}>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+
+  handleClick = (e, data) => {
+    if (data) {
+      console.log("hi")
+      this.createModal(this.props);
     }
 
-    createModal = () => {
-        return (
-            <Modal
-                show={this.state.showModal}
-                close={() => this.setState({showModal: false})}
-                size="lg"
-                //aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title></Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <h4>Are you sure to Logout?</h4>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={() => this.setState({showModal: false})}>Cancel</Button>
-                </Modal.Footer>
-            </Modal>
-        );
-    }
+  }
 
-    render() {
-        const {name, shares, avgPrice, ticker, totalValue} = this.props
+  render() {
+    const { name, shares, avgPrice, ticker, totalValue } = this.props
 
-        return (
-            <>
-                <div className='modal'>
-                    {this.createModal()}
-                </div>
-                <div className='box' onClick={(e) => this.setState({showModal: true})}>
-                    <div className='sb-flex-col'>
-                        <div className='name'>{name}</div>
-                        <div className='share'>{shares} {getNumberOfShares(shares)}</div>
-                        <div className='price'>{avgPrice}</div>
-                    </div>
-                    <div className='ticker'>{ticker}</div>
-                </div>
-            </>
-        )
-    }
+    // function openTheModal(showModal){
+    //   if(showModal){
+    //     return 
+    //   }
+    // }
+
+    return (
+      // onClick={((e) => this.handleClick(e, this.state.showModal))}
+      <>
+        {this.createModal()}
+        <div className='box' onClick={(e) => this.setState({ showModal: true })}>
+          <div className='sb-flex-col'>
+            <div className='name'>{name}
+
+              {/* <SecuritiesModal show={this.state.showModal} close={() => this.setState({showModal: false})} /> */}
+
+            </div>
+            <div className='share'>{shares} {getNumberOfShares(shares)}</div>
+            <div className='price'>{avgPrice}</div>
+          </div>
+          <div className='ticker'>{ticker}</div>
+        </div>
+      </>
+    )
+  }
 
 }
 
